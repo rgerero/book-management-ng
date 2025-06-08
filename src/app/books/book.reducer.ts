39d8089bed079f32ce67a10d59a8af8bb83efed5
alfredryangerero <alfredrrg-v@crimsonlogic.com>
2025-06-09 06:40:08 +0800

    create components to dispatch actions

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -6,6 +6,6 @@ export const initialState: ReadonlyArray<Book> = [];
 
 export const BookReducer = createReducer(
   initialState,
-  on(AddBook, (state, book) => [...state, book]),
+  on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
   on(RemoveBook, (state, { bookId }) => state.filter(book => book.id !== bookId))
 );
